74d7a0b348393ca2b267c18faf01f039f5cc2901
rotba <rotba@post.bgu.ac.il>
2018-09-19 17:14:00 +0300

    nop

--- Test.py
+++ Test.py
@@ -272,8 +272,8 @@ class TestMain(unittest.TestCase):
         Main.repo.git.checkout(commit.hexsha)
         tests_paths = Main.get_tests_paths_from_commit(commit)
         res = Main.extract_bugs(issue, commit, tests_paths)
-        for bug in res:
-            if bug.test.get_id() == exp_testcase_id and bug.desc == my_bug.created_msg:
+        for bug in res[0]:
+            if bug.test.get_id() == exp_testcase_id and bug.desctiption == my_bug.created_msg:
                 return
         self.fail('Did not extracted the bug of testcase -'+ exp_testcase_id)
 
