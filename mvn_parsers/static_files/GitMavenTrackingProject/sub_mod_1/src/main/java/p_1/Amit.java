128057052b731e2851afb311db41f1fcfaeba070
rotba <rotba@post.bgu.ac.il>
2018-09-20 11:23:12 +0300

    static_files change

---
+++ mvn_parsers/static_files/GitMavenTrackingProject/sub_mod_1/src/main/java/p_1/Amit.java
@@ -0,0 +1,7 @@
+package p_1;
+
+public class Amit {
+    public int hoo(){return 5;}
+    public int boo(){return 5;}
+    public int foo(){return 0;}
+}
---
+++ mvn_parsers/static_files/GitMavenTrackingProject/sub_mod_1/src/test/java/p_1/AmitTest.java
@@ -0,0 +1,17 @@
+package p_1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AmitTest {
+
+    Amit a = new Amit();
+    @Test
+    void hoo() {
+    }
+    @Test
+    void fooTest() {
+        assertEquals(0,a.foo());
+    }
+}
