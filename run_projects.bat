0d961e3378a53d07310f5d9cf86f386ad2cdd884
rotba <rotba@post.bgu.ac.il>
2018-10-10 18:02:25 +0300

    nop

--- run_projects.bat
+++ run_projects.bat
@@ -1,3 +1,10 @@
-python Main.py https:\github.com\apache\tika http:\issues.apache.org\jira\projects\TIKA
 python Main.py https:\github.com\apache\commons-math http:\issues.apache.org\jira\projects\MATH
 python Main.py https:\github.com\apache\commons-lang http:\issues.apache.org\jira\projects\LANG
+python Main.py https:\github.com\apache\tika http:\issues.apache.org\jira\projects\TIKA
+python Main.py https:\github.com\apache\camel http:\issues.apache.org\jira\projects\CAMEL
+python https:\github.com\apache\accumulo http:\issues.apache.org\jira\projects\ACCUMULO
+python https:\github.com\apache\flink http:\issues.apache.org\jira\projects\FLINK
+python https:\github.com\apache\maven http:\issues.apache.org\jira\projects\MNG
+python https:\github.com\apache\wicket http:\issues.apache.org\jira\projects\WICKET
+python https:\github.com\apache\jackrabbit-oak http:\issues.apache.org\jira\projects\OAK
+python https:\github.com\apache\commons-logging http:\issues.apache.org\jira\projects\LOGGING
