06b9e6eb8787efc7fec8876cf1e088394efe7bef
Amir Elmishali <amir9979@gmail.com>
2020-07-04 15:01:49 +0300

    main

--- Main.py
+++ Main.py
@@ -228,6 +228,7 @@ def get_relevant_tests(changed_classes_diffs, tests_paths, commit):
 									   os.path.normpath(x.lower()).replace(".java", "").replace(os.path.sep, ".")),
 							repo.git.ls_files().split()))
 	diffs_packages = map(lambda x: os.path.normpath(x.replace(".java", "")).replace(os.sep, ".").split("java.")[1].lower(), changed_classes_diffs)
+	diffs_packages = map(lambda x: ".".join(x.split('.')[:-1]), diffs_packages)
 	tests_paths.extend(
 		list(map(lambda x: x[0], filter(lambda x: any(map(lambda y: y in x[1], diffs_packages)), test_files))))
 	commit_tests_object = list(map(lambda t_path: TestObjects.TestClass(t_path, commit.repo.working_dir),
@@ -838,6 +839,6 @@ def main(project_name, minor_ind, major_ind):
 
 if __name__ == '__main__':
 	main(*sys.argv[1:])
-	for p in settings.projects:
-		if "common" in str(settings.projects[p]):
-			generate_data(p)
+	# for p in settings.projects:
+	# 	if "common" in str(settings.projects[p]):
+	# 		generate_data(p)
